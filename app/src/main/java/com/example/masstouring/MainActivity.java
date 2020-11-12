10121ca7f2da79776e7caaf24803caa91c838f3d
cony <cony@cony>
2020-11-12 20:54:39 +0900

    first commit

--- app/src/main/java/com/example/masstouring/MainActivity.java
+++ app/src/main/java/com/example/masstouring/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Intent i = new Intent(MainActivity.this, MapsFragment.class);
+                        Intent i = new Intent(MainActivity.this, MapActivity.class);
                         startActivity(i);
                     }
                 }
