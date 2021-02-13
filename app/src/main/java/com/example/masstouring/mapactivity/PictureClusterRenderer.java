52e2320feba238e80a87346b21fccd443fb04320
cony <38028734+cony26@users.noreply.github.com>
2021-02-13 14:39:25 +0900

    implement clusterItemRendering

--- app/src/main/java/com/example/masstouring/mapactivity/PictureClusterRenderer.java
+++ app/src/main/java/com/example/masstouring/mapactivity/PictureClusterRenderer.java
@@ -2,8 +2,10 @@ package com.example.masstouring.mapactivity;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
@@ -20,19 +22,30 @@ import com.google.maps.android.ui.IconGenerator;
 
 public class PictureClusterRenderer extends DefaultClusterRenderer<Picture> {
     private final IconGenerator oClusterIconGenerator;
+    private final IconGenerator oClusterItemIconGenerator;
     private final ImageView oClusterImageView;
+    private final ImageView oClusterItemImageView;
     private Context oContext;
     private ClusterManager<Picture> oClusterManager;
+    private final int oDimension;
     public PictureClusterRenderer(Context context, GoogleMap map, ClusterManager<Picture> clusterManager) {
         super(context, map, clusterManager);
         oContext = context;
         oClusterManager = clusterManager;
         oClusterIconGenerator = new IconGenerator(context);
+        oClusterItemIconGenerator = new IconGenerator(context);
 
-        View multiProfile = LayoutInflater.from(context).inflate(R.layout.cluster_layout, null);
-        oClusterIconGenerator.setContentView(multiProfile);
+        View clusterLayout = LayoutInflater.from(context).inflate(R.layout.cluster_layout, null);
+        oClusterIconGenerator.setContentView(clusterLayout);
         oClusterIconGenerator.setTextAppearance(R.id.amu_text);
-        oClusterImageView = multiProfile.findViewById(R.id.cluster_image);
+        oClusterImageView = clusterLayout.findViewById(R.id.cluster_image);
+
+        oClusterItemImageView = new ImageView(context);
+        oDimension = (int)context.getResources().getDimension(R.dimen.cluster_item_image);
+        oClusterItemImageView.setLayoutParams(new ViewGroup.LayoutParams(oDimension,oDimension));
+        int padding = (int)context.getResources().getDimension(R.dimen.cluster_item_padding);
+        oClusterItemImageView.setPadding(padding, padding, padding, padding);
+        oClusterItemIconGenerator.setContentView(oClusterItemImageView);
     }
 
     @Override
