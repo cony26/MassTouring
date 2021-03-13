fbfef9e5d2cde0b66408927ec7329c19d1b24a58
cony <38028734+cony26@users.noreply.github.com>
2021-03-13 14:19:29 +0900

    create Rect list for cluster

--- app/src/main/java/com/example/masstouring/mapactivity/ClusterDistributer.java
+++ app/src/main/java/com/example/masstouring/mapactivity/ClusterDistributer.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.masstouring.R;
 import com.example.masstouring.common.LoggerTag;
 import com.google.maps.android.clustering.Cluster;
 import com.google.maps.android.clustering.ClusterManager;
@@ -20,12 +21,14 @@ public class ClusterDistributer{
     private final ClusterManager<Picture> oClusterManager;
     private final ClusterDistributedView oClusterDistributedView;
     private final MapActivtySharedViewModel oMapActivitySharedViewModel;
+    private final int oClusterSquarePx;
 
     ClusterDistributer(Context aContext, ClusterManager<Picture> aClusterManager, MapActivtySharedViewModel aViewModel){
         oClusterDistributedView = new ClusterDistributedView(aContext);
         oContext = aContext;
         oClusterManager = aClusterManager;
         oMapActivitySharedViewModel = aViewModel;
+        oClusterSquarePx = (int)aContext.getResources().getDimension(R.dimen.cluster_item_image);
     }
 
     public void distribute(Cluster<Picture> aCluster){
@@ -33,41 +36,27 @@ public class ClusterDistributer{
 
         //get item bitmap
         List<Bitmap> bitmapList = aCluster.getItems().stream()
-                .map(picture -> picture.getBitmapSyncly(oContext, 200,200))
+                .map(picture -> picture.getBitmapSyncly(oContext, oClusterSquarePx / 2,oClusterSquarePx / 2))
                 .collect(Collectors.toList());
 
         //calculate the layout of each item for distribution
         ViewGroup parent = (ViewGroup)oClusterDistributedView.getParent();
+        int viewCenterX = parent.getMeasuredWidth() / 2;
+        int viewCenterY = parent.getMeasuredHeight() / 2;
 
         List<Rect> distributedRectList = new ArrayList<>();
-        int viewCenterX = parent.getMeasuredWidth() / 2;
-        int viewCenterY = parent.getMeasuredWidth() / 2;
-
-        Log.e(LoggerTag.CLUSTER, String.format("viewCenter (x,y) = (%d, %d)", viewCenterX, viewCenterY));
-
         int size = bitmapList.size();
         int halfSize = size / 2;
         for(int i = 0; i < halfSize; i++){
            int r = 200;
-           double theta = Math.PI / 3 * i;
-           int centerX = (int) (r * Math.cos(theta)) + viewCenterX;
-           int centerY = (int) (r * Math.sin(theta)) + viewCenterY;
-           int w = bitmapList.get(i).getWidth();
-           int h = bitmapList.get(i).getHeight();
-
-            Log.e(LoggerTag.CLUSTER, String.format("(w,h,i) = (%d, %d, %d)", w, h, i));
-            distributedRectList.add(new Rect(centerX - w / 2, centerY - h / 2, centerX + w / 2, centerY + h /2));
+           double theta = Math.PI / 6 * i;
+            distributedRectList.add(createRectOnCircle(bitmapList.get(i), viewCenterX, viewCenterY, r, theta));
         }
 
         for(int i = halfSize; i < size; i++){
             int r = 400;
-            double theta = Math.PI / 3 * i;
-            int centerX = (int) (r * Math.cos(theta)) + viewCenterX;
-            int centerY = (int) (r * Math.sin(theta)) + viewCenterY;
-            int w = bitmapList.get(i).getWidth();
-            int h = bitmapList.get(i).getHeight();
-            Log.e(LoggerTag.CLUSTER, String.format("(w,h,i) = (%d, %d, %d)", w, h, i));
-            distributedRectList.add(new Rect(centerX - w / 2, centerY - h / 2, centerX + w / 2, centerY + h /2));
+            double theta = Math.PI / 6 * (i - halfSize);
+            distributedRectList.add(createRectOnCircle(bitmapList.get(i), viewCenterX, viewCenterY, r, theta));
         }
 
         for(Rect rect : distributedRectList){
@@ -78,6 +67,14 @@ public class ClusterDistributer{
         oClusterDistributedView.drawItems(bitmapList, distributedRectList);
 
         oMapActivitySharedViewModel.getIsClusterDistributed().setValue(true);
+    }
+
+    private Rect createRectOnCircle(Bitmap aBitmap, int aCircleCenterX, int aCircleCenterY, int aRadius, double aTheta){
+        int centerX = (int) (aRadius * Math.cos(aTheta)) + aCircleCenterX;
+        int centerY = (int) (aRadius * Math.sin(aTheta)) + aCircleCenterY;
+        int w = aBitmap.getWidth();
+        int h = aBitmap.getHeight();
+        return new Rect(centerX - w / 2, centerY - h / 2, centerX + w / 2, centerY + h /2);
     }
 
     public void cleanUp(){
